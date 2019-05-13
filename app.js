d45103d352346d12f13f2d02e97659a155b4c7b0
9080tom <t.thickett@hotmail.co.uk>
2019-05-13 10:49:26 +0100

    added cors

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ const {
   handleCustomErrors,
   handlePsqlErrors
 } = require("./errors");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
